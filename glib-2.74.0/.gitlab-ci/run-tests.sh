6bdad95273459ba5fb8b9e7ff53860ecf3cd5a55
sk4ndal-commits <73082008+sk4ndal-commits@users.noreply.github.com>
2025-04-20 04:12:40 +0200

    Delete glib-2.74.0 directory

--- glib-2.74.0/.gitlab-ci/run-tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-meson test \
-        -C _build \
-        --timeout-multiplier "${MESON_TEST_TIMEOUT_MULTIPLIER}" \
-        --no-suite flaky \
-        "$@"
--- glib-2.74.0/CODE_OF_CONDUCT.md
+++
@@ -1,5 +0,0 @@
-Code of Conduct
-===
-
-GLib follows the GNOME Code of Conduct, which is documented here:
-https://wiki.gnome.org/Foundation/CodeOfConduct
